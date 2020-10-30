460b9f9a74bed509acec82e82b50c364a175141e
Sander Lindberg <sander.b.lindberg@gmail.com>
2020-10-30 14:46:23 +0100

    Fix Spotify

--- CBR-system/Spotify.py
+++ CBR-system/Spotify.py
@@ -1,4 +1,5 @@
 import requests
+from difflib import get_close_matches
 
 class Spotify:
 
@@ -14,12 +15,12 @@ class Spotify:
         })
         return resp.json()['access_token'], resp.json()['token_type']
 
-    def search(self, query, qtype):
+    def search(self, query, artist, qtype):
+        print(query)
         url = f'https://api.spotify.com/v1/search'
         params = {
             'query': query.replace(' ', '+'),
             'type': qtype,
-            'limit': 1,
             'market': 'NO'
         }
 
@@ -27,10 +28,45 @@ class Spotify:
             'Authorization': f'Bearer {self.access_token}'
         }
 
-        return requests.get(url, params=params, headers=headers)
+        res = requests.get(url, params=params, headers=headers).json()
 
-    def getAudioFeatures(self, searchResponse):
-        track_id = searchResponse.json()['tracks']['items'][0]['id']
+        if 'tracks' not in res.keys():
+            return False
+        
+        tracks = res['tracks']['items']
+
+        artists = []
+        
+        for track in tracks:
+            for artist_in_track in track['artists']:
+                 artists.append(artist_in_track['name'].lower())
+        
+        artists = list(set(artists))
+
+        corrected_artist = ''.join(get_close_matches(artist, artists, n=1)).lower()
+
+        track_id =  None # list(filter(lambda x: x['atrists']['name'].lower() == atrist and x['name'].lower() == query, tracks))
+        selected_track = None
+
+        for track in sorted(tracks, key=lambda x: x['album']['release_date']):
+            breaked = False
+            for artist in track['artists']:
+                if artist['name'].lower() == corrected_artist:
+                    if query.lower() in track['name'].lower():
+                        track_id = track['id']
+                        selected_track = track
+                        breaked = True
+                        break
+            if breaked:
+                break
+
+        if track_id is None:
+            return False
+        
+        return track_id, track
+
+
+    def getAudioFeatures(self, track_id):
         url = 'https://api.spotify.com/v1/audio-features/' + track_id
         headers = {
             'Authorization': f'Bearer {self.access_token}'
